a6e531e394519f5665a32d68809cbe93d8a70955
Pavelkarkev <klol12675@gmail.com>
2025-09-10 12:13:00 +0300

    Add files via upload

---
+++ task1edited.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, d;
+	
+	std::cout << "Введите a,b,d" << std::endl;
+	if (!(std::cin >> a >> b >> d)) {
+		std::cout << "wont work";
+		std::exit(1);
+	}
+	else {
+		for (int i = std::min(a, b); i <= std::max(a, b); i += abs(d)) {
+			//if (d >= 0) {
+				if (i % 3 == 0) {
+					std::cout << i << " ";
+				}
+			//}
+			//if (d < 0) {
+			//	if (i % 3 == 0) {
+			//		std::cout << i << " ";
+			//	}
+			//}
+		}
+	}
+}
+
+			
+
+	
+
+
