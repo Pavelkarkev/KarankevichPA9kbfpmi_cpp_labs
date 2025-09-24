e936c0e7ecbe987c25d29dd97ad61d0a19b043e6
Pavelkarkev <klol12675@gmail.com>
2025-09-24 10:46:56 +0300

    fixed my program

--- semester_1/lab2_integer_arithmetics/сhislamercenna.cpp
+++ semester_1/lab2_integer_arithmetics/сhislamercenna.cpp
@@ -1,29 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
 
 int main() {
     int a;
     int b;
     int counter = 0;
+    int n = std::min(a, b);
     std::cout << "enter boundaries a b" << std::endl;
     if (!(std::cin >> a >> b)) {
         std::cout << "wont work";
         std::exit(1);
     }
-    if (a < 0) {
-        a = 1;
+    if (n < 0) {
+         n = 1;
     }
-    if (a && b < 0) {
+    if (n && std::max(a,b)< 0) {
         std::cout << "numbers should be positive";
         std::exit(0);
     }
-    for (int i = std::min(a, b); i <= std::max(a, b); i++) {
-        
+    for (int i = n; i <= std::max(a, b); i++) {
+
         int powerof2 = pow(2, counter);
         if ((i + 1) % powerof2 == 0) {
-            std::cout << i<<" ";
             counter++;
         }
+            if (powerof2>std::min(a,b) && (i + 1) % powerof2 == 0) {
+                std::cout << i << " ";
+            }
         
     }
 
