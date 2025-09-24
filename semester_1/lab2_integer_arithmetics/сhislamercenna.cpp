d85ea167ee81f45dd3cdc9a805de12145f953e9a
Pavelkarkev <klol12675@gmail.com>
2025-09-24 10:13:57 +0300

    language fix

--- semester_1/lab2_integer_arithmetics/сhislamercenna.cpp
+++ semester_1/lab2_integer_arithmetics/сhislamercenna.cpp
@@ -5,7 +5,7 @@ int main() {
     int a;
     int b;
     int counter = 0;
-    std::cout << "Введите границы отрезка a b" << std::endl;
+    std::cout << "enter boundaries a b" << std::endl;
     if (!(std::cin >> a >> b)) {
         std::cout << "wont work";
         std::exit(1);
@@ -14,7 +14,7 @@ int main() {
         a = 1;
     }
     if (a && b < 0) {
-        std::cout << "Числа Мерсенна должны быть натуральными";
+        std::cout << "numbers should be positive";
         std::exit(0);
     }
     for (int i = std::min(a, b); i <= std::max(a, b); i++) {
