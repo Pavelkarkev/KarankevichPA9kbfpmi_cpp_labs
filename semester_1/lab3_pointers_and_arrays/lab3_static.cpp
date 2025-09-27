7814af9f8b5f95fee676fd4e34d1d148a434eafb
Pavelkarkev <klol12675@gmail.com>
2025-09-28 00:59:48 +0300

    Task with static arrays was done

--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,7 +1,104 @@
+#include <iostream>
+#include<cmath>
+#include<random>
 
-// solve task with usage of
-// static arrays
-int main() {
+int main()
+{
+    int n;
+    const int maxlength = 100;
 
-    return 0;
+    std::cout << "enter the length of the array" << std::endl;
+    if (!(std::cin >> n) || (n < 0)) {
+        std::cout << "please enter natural numbers";
+        std::exit(1);
+    }
+    
+    if (n > maxlength) {
+        std::cout << "exceeded size limit.Please enter a size less than 100";
+        std::exit(1);
+    }
+   
+    int* p = new int[n];
+    int* valuevgetback = new int[n];
+    int forrand=0;
+    std::cout << "for random numebrs type 67" << std::endl;
+    std::cin >> forrand;
+    if(forrand==67){
+        std::cout << "SIX SEVEEEEN"<<std::endl;
+        int a = 0;
+        int b = 0;
+        std::cout << "enter two whole numbers that will be boundaries of generation" << std::endl;
+        std::cin >> a >> b;
+        std::mt19937 gen;
+        std::uniform_int_distribution<int> dist(std::min(a, b), std::max(a, b));   
+        for (int i = 0; i < n; ++i) {
+            p[i] = dist(gen);  
+        }          
+    }
+    else {
+        std::cout << "enter the elements of the array" << std::endl;
+        for (int i = 0; i < n; ++i) {
+            if (!(std::cin >> p[i])) {
+                std::cout << "no chepuha please.Enter whole numbers";
+                std::exit(1);
+            }
+           
+        }
+    }
+        int min = 0;
+        int max = 0;
+        for (int i = 1; i < n; ++i) {
+
+            if (p[i] < p[min]) {
+                min = i;
+            }
+            if (p[i] > p[max]) {
+                max = i;
+            }
+        }
+        p[min] ^= (p[max] ^= (p[min] ^= p[max]));
+        for (int i = 0; i < n; ++i) {
+            std::cout << p[i] << " ";
+            valuevgetback[i] = p[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            p[i] = abs(p[i]);
+        }
+        int absmin = 0;
+        for (int i = 1; i < n; ++i) {
+            if (p[i] < p[absmin]) {
+                absmin = i;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            p[i] = valuevgetback[i];
+        }
+        int sum = 0;
+        for (int k = absmin+1; k < n; ++k) {
+            sum+=p[k];
+        }
+        std::cout << "summary of the numbers is:" << sum << std::endl;
+        for (int i = 0; i < n; ++i) {
+            if (p[i] < 0) {
+                p[i] *= p[i];
+            }
+        }
+        int temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (p[j] > p[j + 1]) {
+                    temp = p[j];
+                    p[j] = p[j + 1];
+                    p[j + 1] = temp;
+                }
+            }
+        }
+        std::cout << "our array but with squares instead of negative numbers:";
+        for (int i = 0; i < n; i++) {
+            std::cout << p[i] << " ";
+        }
+    delete [] p;
+    delete [] valuevgetback;
+    std::cout << ":)";
+    return 0;   
 }
