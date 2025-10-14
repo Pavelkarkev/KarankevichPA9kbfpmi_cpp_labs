a61016207cf9e8137e17eea3cf35ae96e2164652
Pavelkarkev <klol12675@gmail.com>
2025-10-14 23:55:19 +0300

    Updated

--- semester_1/lab3_pointers_and_arrays/lab3_static.cpp
+++ semester_1/lab3_pointers_and_arrays/lab3_static.cpp
@@ -1,103 +1,139 @@
 #include <iostream>
 #include<cmath>
 #include<random>
-
-int main()
-{
-    int n;
-    const int maxlength = 100;
-
-    std::cout << "enter the length of the array" << std::endl;
-    if (!(std::cin >> n) || (n < 0)) {
-        std::cout << "please enter natural numbers";
+void inputnumberwithcheck(int&length) {
+    std::cout << "enter the length of the array";
+    if (!(std::cin >> length)||length<=0) {
+        std::cout<< "no chepuha please.enter whole numbers";
         std::exit(1);
     }
-
-    if (n > maxlength) {
+}
+void checkiflengthexceeded(int& length,const int&maxlength) {
+    if (length > maxlength) {
         std::cout << "exceeded size limit.Please enter a size less than 100";
         std::exit(1);
     }
-
-    int* p = new int[n];
-    int forrand = 0;
-    std::cout << "for random numebrs type 67" << std::endl;
-    std::cin >> forrand;
-    int a = 0;
-    int b = 0;
+}
+void boundariesinputwithcheck(int& a,int& b,int*arr) {
     std::cout << "enter two whole numbers that will be boundaries of generation" << std::endl;
     if (!(std::cin >> a >> b)) {
-        std::cout << "no chepuha please.Enter whole numbers";
-        delete[] p;
+        std::cout << "no chepuha please.enter whole numbers";
         std::exit(1);
     }
-    if (forrand == 67) {
-        std::cout << "our array:";
-        std::mt19937 gen;
-        std::uniform_int_distribution<int> dist(std::min(a, b), std::max(a, b));
-        for (int i = 0; i < n; ++i) {
-            p[i] = dist(gen);
-            std::cout <<p[i] << " ";
+}
+void inputwithcheckchoice(int& forrand,int*arr) {
+    std::cout << "for random numebrs type 67,for manual input type anything else" << std::endl;
+    if (!(std::cin >> forrand)) {
+        std::cout << "no chepuha please.enter normal numbers";
+        std::exit(1);
+    }
+}
+void genrandomforarraywithboundaries(int& length, int* arr,int&a,int&b) {
+    std::cout << "enter two whole numbers that will be boundaries of generation" << std::endl;
+    if (!(std::cin >> a >> b)) {
+        std::cout << "no chepuha please.enter whole numbers";
+        std::exit(1);
+    }
+    std::mt19937 gen;
+    std::uniform_int_distribution<int> dist(std::min(a, b), std::max(a, b));
+    for (int i = 0; i < length; ++i) {
+        arr[i] = dist(gen);
+    }
+}
+void printarray(int& length, int* arr) {
+    for (int i = 0; i < length; ++i) {
+        std::cout << arr[i] << " ";
+    }
+}
+void manualinputingwithcheck(int& length,int*arr) {
+    std::cout << "enter the elements of the array" << std::endl;
+    for (int i = 0; i < length; ++i) {
+        if (!(std::cin >> arr[i])) {
+            std::cout << "no chepuha please.enter whole numbers";
+            std::exit(1);
         }
     }
-    else {
-        std::cout << "enter the elements of the array" << std::endl;
-        for (int i = 0; i < n; ++i) {
-            if (!(std::cin >> p[i])) {
-                std::cout << "no chepuha please.Enter whole numbers";
-                delete[] p;
-                std::exit(1);
-            }
-
-        }
-    }
-    int min = 0;
-    int max = 0;
-    for (int i = 1; i < n; ++i) {
-
-        if (p[i] < p[min]) {
+}
+void findingthebiggestandsmallestnumbers(int& min, int& max,int*arr,int&length) {
+    for (int i = 1; i < length; ++i) {
+        if (arr[i] < arr[min]) {
             min = i;
         }
-        if (p[i] > p[max]) {
+        if (arr[i] > arr[max]) {
             max = i;
         }
     }
-    p[min] ^= (p[max] ^= (p[min] ^= p[max]));
-    std::cout << "sorted array:";
-    for (int i = 0; i < n; ++i) {
-        std::cout <<p[i] << " ";
-    }
-    p[min] ^= (p[max] ^= (p[min] ^= p[max]));
-    int absmin = 0;
-    for (int i = 1; i < n; ++i) {
-        if (std::abs(p[i]) < std::abs(p[absmin])) {
+}
+void swap(int& min, int& max,int*arr) {
+    arr[min] ^= (arr[max] ^= (arr[min] ^= arr[max]));
+}
+int thesmalestabsolute(int& absmin, int& length, int* arr) {
+    for (int i = 1; i < length; ++i) {
+        if (std::abs(arr[i]) < std::abs(arr[absmin])) {
             absmin = i;
         }
     }
-    int sum = 0;
-    for (int k = absmin + 1; k < n; ++k) {
-        sum += p[k];
+    return absmin;
+}
+void summaryofelements(int& sum, int& length, int* arr, int& absmin) {
+    for (int k = absmin + 1; k < length; ++k) {
+        sum += arr[k];
     }
-    std::cout << "summary of the numbers is:" << sum << std::endl;
-    for (int i = 0; i < n; ++i) {
-        if (p[i] < 0) {
-            p[i] *= p[i];
+}
+void negativetosquares(int& length, int* arr) {
+    for (int i = 0; i < length; ++i) {
+        if (arr[i] < 0) {
+            arr[i] *= arr[i];
         }
     }
+}
+void bubblesorting(int& length, int* arr) {
     int temp;
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (p[j] > p[j + 1]) {
-                temp = p[j];
-                p[j] = p[j + 1];
-                p[j + 1] = temp;
+    for (int i = 0; i < length - 1; i++) {
+        for (int j = 0; j < length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    std::cout << "our array but with squares instead of negative numbers:";
-    for (int i = 0; i < n; i++) {
-        std::cout << p[i] << " ";
+}
+int main()
+{
+    int length = 0;
+    const int maxlength = 100;
+    inputnumberwithcheck(length);
+    checkiflengthexceeded(length,maxlength);
+    int arr[maxlength];
+    int forrand = 0;
+    inputwithcheckchoice(forrand,arr);
+    int a = 0;
+    int b = 0;
+    if (forrand == 67) {
+        genrandomforarraywithboundaries(length, arr, a, b);
+        std::cout << "our array:";
+        printarray(length, arr);
     }
-    delete[] p;
+    else {
+        manualinputingwithcheck(length, arr);
+    }
+    int min = 0;
+    int max = 0;
+    findingthebiggestandsmallestnumbers(min, max, arr, length);
+    swap(min, max, arr);
+    std::cout << "sorted array:";
+    printarray(length, arr);
+    swap(min, max, arr);
+    int absmin = 0;
+    thesmalestabsolute(absmin, length, arr);
+    int sum = 0;
+    summaryofelements(sum, length, arr, absmin);
+    std::cout << "summary of the unsorted array's numbers is:" << sum << std::endl;
+    negativetosquares(length, arr);
+    bubblesorting(length, arr);
+    std::cout << "our array but with squares instead of negative numbers:";
+    printarray(length, arr);
     std::cout << ":)";
     return 0;
 }
