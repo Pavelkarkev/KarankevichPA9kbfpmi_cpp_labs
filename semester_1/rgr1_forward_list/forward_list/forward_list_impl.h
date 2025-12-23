00b2f283af1c8ef3c35d5a37d786de39be572c08
Pavelkarkev <klol12675@gmail.com>
2025-12-23 23:42:42 +0300

    beauty fix

--- semester_1/rgr1_forward_list/forward_list/forward_list_impl.h
+++ semester_1/rgr1_forward_list/forward_list/forward_list_impl.h
@@ -5,18 +5,16 @@
 #include <iostream>
 
 class ForwardList {
-
 private:
     struct Node {
         int32_t value_;
         Node* next_;
-
         explicit Node(int value) : value_(value), next_(nullptr) {
         }
-
     };
-    Node* front;
-    size_t size;
+
+    Node* front_;
+    size_t size_;
 
 public:
     class ForwardListIterator {
@@ -31,81 +29,34 @@ public:
         }
 
         ForwardListIterator& operator++();
-
-
         ForwardListIterator operator++(int);
-
-
         bool operator==(const ForwardListIterator& other) const;
-
-
         bool operator!=(const ForwardListIterator& other) const;
-
-
         reference operator*() const;
-
-
         pointer operator->();
 
-        
     private:
         Node* position_;
     };
 
-
     ForwardListIterator begin();
-
-    
     ForwardListIterator end();
- 
     ForwardListIterator begin() const;
-
-    
     ForwardListIterator end() const;
 
-
-   
     ForwardList();
-
-   
     ForwardList(const ForwardList& rhs);
-
-    
     ForwardList(size_t count, int32_t value);
-
-
     ForwardList(std::initializer_list<int32_t> init);
-
-    
     ForwardList& operator=(const ForwardList& rhs);
-
-   
     ~ForwardList();
 
-  
     void PushFront(int32_t value);
-
-
     void PopFront();
-
-   
     void Remove(int32_t value);
-
-    
     void Clear();
-
- 
     bool FindByValue(int32_t value);
-
-    
     void Print(std::ostream& out);
-
-    
     int32_t Front() const;
-
-  
     size_t Size() const;
-
-private:
-    
 };
